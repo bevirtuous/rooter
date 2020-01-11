b4e1556ec382fef17e834421e8668637d105cd19
Richard Gorman <richardgorman@outlook.com>
2020-01-11 21:17:07 +0100

    Added useQuery to public API

--- src/react/index.js
+++ src/react/index.js
@@ -2,6 +2,7 @@ export { default as Route } from './Route';
 export { default as Router } from './Router';
 export { default as useHistory } from './useHistory';
 export { default as useParams } from './useParams';
+export { default as useQuery } from './useQuery';
 export { default as useRoute } from './useRoute';
 export * from './context';
 export * from './events';
