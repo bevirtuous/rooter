0035af96c120a175de489f448d41cd9ba4e5a4c5
Richard Gorman <richardgorman@outlook.com>
2020-03-08 23:20:41 +0100

    Added more approachable entries to core API

--- src/core/index.js
+++ src/core/index.js
@@ -1,12 +1,14 @@
 import router from './Router';
 
 export const history = {
+  back: router.pop,
   current: router.getCurrentRoute,
   push: router.push,
+  go: router.push,
   pop: router.pop,
   replace: router.replace,
   reset: router.reset,
-  update: router.update,
+  setMeta: router.update,
 };
 
 export const register = router.register;
--- src/core/spec.js
+++ src/core/spec.js
@@ -2,12 +2,14 @@ import { history } from './index';
 import router from './Router';
 
 describe('Core API', () => {
-  it('should export correct API', () => {
+  it('should map to router internal methods', () => {
+    expect(history.go).toBe(router.push);
     expect(history.push).toBe(router.push);
+    expect(history.back).toBe(router.pop);
     expect(history.pop).toBe(router.pop);
     expect(history.replace).toBe(router.replace);
     expect(history.reset).toBe(router.reset);
-    expect(history.update).toBe(router.update);
     expect(history.current).toBe(router.getCurrentRoute);
+    expect(history.setMeta).toBe(router.update);
   });
 });
