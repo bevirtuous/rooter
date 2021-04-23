072b0c63e5b7f4f7cc3a5acc072bc7a045e2aae2
Richard Gorman <richardgorman@outlook.com>
2021-04-23 10:00:03 +0200

    Corrected export

--- src/react/index.js
+++ src/react/index.js
@@ -1,5 +1,5 @@
 export { default as Route } from './Route';
 export { default as useHistory } from './useHistory';
 export { default as useLocation } from './useLocation';
-export { default as usParamsy } from './useParams';
+export { default as useParams } from './useParams';
 export { default as usePath } from './usePath';
